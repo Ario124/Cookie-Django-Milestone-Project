f9a7d20457994f35f42b5d0913d4f293d2ab5124
Ubuntu <ubuntu@ip-172-31-88-141.ec2.internal>
2019-12-19 19:53:16 +0000

    Updated css, disabled debug mode.

--- cookieproject/settings.py
+++ cookieproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['e8e956d19f0f4a28bc56646bd5e716ad.vfs.cloud9.us-east-1.amazonaws.com', 'cookie-milestone.herokuapp.com']
 
